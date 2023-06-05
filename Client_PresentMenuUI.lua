8c4cedd5264452ddc87377263423f8f4b29b7591
larenz jackson <romanlegion15@gmail.com>
2023-06-05 12:41:59 -0500

    Update Client_PresentMenuUI.lua

--- Client_PresentMenuUI.lua
+++ Client_PresentMenuUI.lua
@@ -1,5 +1,8 @@
 function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game, close)
 
+    	setMaxSize(450, 320);
+
+    
 local vert = UI.CreateVerticalLayoutGroup(rootParent);
     local Chartracker = UI.CreateTextInputField(vert).SetPlaceholderText(" Name of Character                       ").SetFlexibleWidth(1).SetCharacterLimit(30).SetPreferredWidth(30)
 
